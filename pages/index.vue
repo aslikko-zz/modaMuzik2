d7b8fc7a6b9036e5ae163ecf0c31c4b7a8d926bf
aslikko <aslikko@gmail.com>
2020-12-06 08:03:47 +0300

    first version with nuxt

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+<SideMenu />
+    <MainSlider />
+  </div>
+</template>
+
+<script>
+  import Header from '~/components/SideMenu'
+  import Menu from '~/components/MainSlider'
+  export default {
+    components: { Menu, Header  }
+  }
+</script>
+
+<style>
+
+</style>
